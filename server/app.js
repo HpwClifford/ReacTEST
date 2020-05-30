d03443e35f79e9b46e8d240140373b047db7c22b
Harry Clifford <harryclifford@Harrys-MacBook-Pro.local>
2020-05-30 10:45:57 -0400

    react rendering

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.resolve(__dirname, '../src')));
+
+app.listen(port, () => console.log(`server listening on port  ${port}`));
+
+module.exports = app;
---
+++ src/hook.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable wrap-iife */
+/* eslint-disable func-names */
+/* eslint-disable no-underscore-dangle */
+function hook() {
+  const devTools = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+  devTools.onCommitFiberRoot = (function (original) {
+    return function (...args) {
+      const fiberDOM = args[1];
+      const rootNode = fiberDOM.current.stateNode.current;
+      const arr = [];
+      recurse(rootNode.child, arr);
+      console.log(arr[0]);
+      return original(...args);
+    };
+  })(devTools.onCommitFiberRoot);
+}
+
+function recurse(node, parentArr) {
+  const component = {
+    name: '',
+    children: [],
+    state: null,
+    props: null,
+    node,
+  };
+
+  // console.log('stepped', node);
+
+  // get name
+  if (node.type) {
+    if (node.type.name) {
+      component.name = node.type.name;
+    } else {
+      // this is an html element
+      if (node.child) recurse(node.child, parentArr);
+      if (node.sibling) recurse(node.sibling, parentArr);
+      return;
+    }
+  }
+
+  // get state
+  if (node.memoizedState) component.state = node.memoizedState;
+
+  // get props
+  if (node.memoizedProps) component.props = node.memoizedProps;
+
+  component.children = [];
+
+  parentArr.push(component);
+  if (node.child) recurse(node.child, component.children);
+  if (node.sibling) recurse(node.sibling, parentArr);
+}
+
+module.exports = hook;
