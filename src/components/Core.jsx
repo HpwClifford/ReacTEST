576fa25c78798fb6c4847a80bdb2ea30d4a93f7c
Harry Clifford <harryclifford@Harrys-MacBook-Pro.local>
2020-06-11 14:53:48 -0400

    updated core

--- src/components/Core.jsx
+++ src/components/Core.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Worm from './Worm.jsx';
 
 const Core = ({ coreKey }) => {
+  const [test, setTest] = useState([[9]]);
+
   return (
     <div>
       <span>{coreKey}</span>
-      <Worm wormKey={coreKey+2} />
+      <Worm wormKey={coreKey + 2} />
     </div>
-  )
+  );
 };
 
 export default Core;
